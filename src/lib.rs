7b244a421a5bdb461e3f796705929b3ba984a155
Eric Berquist <eric.berquist@gmail.com>
2020-11-26 14:52:51 -0500

    silence warnings from bindgen code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use std::f64::consts::PI;
 
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(non_upper_case_globals)]
+#[allow(unused_variables)]
 mod ffi_bindgen {
     include!(concat!(env!("OUT_DIR"), "/bindings_libint2.rs"));
 }
